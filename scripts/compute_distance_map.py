Fixes distance computation

scripts/compute_distance_map.py
@@ -1,5 +1,5 @@
 """
-Computes the distance between each tooth-tooth pair.
+Computes the distance between each tooth-tooth pair. This should run following swap.
 """
 import glob
 
@@ -9,11 +9,15 @@ from tqdm import tqdm
 from scripts.utils.distance_mapper import DistanceMapper
 
 if __name__ == "__main__":
+    DIR = "processed"  # "final" | "processed"
     JAW = "upper"
     ids = list(map(lambda x: x.split("/")[-1], glob.glob("../data/processed/*")))
     distance_mapper = DistanceMapper()
     for id in tqdm(ids, total=len(ids)):
-        centroids = np.load(f"../data/final/{id}/centroids_{JAW}.npy")
-        labels = np.load(f"../data/final/{id}/labels_{JAW}.npy")
+        centroids = np.load(f"../data/{DIR}/{id}/centroids_{JAW}.npy")
+        try:
+            labels = np.load(f"../data/{DIR}/{id}/labels_{JAW}.npy")
+        except:
+            labels = np.arange(17)
         distance_map = distance_mapper(centroids, labels)
-        np.save(f"../data/final/{id}/distance_map_{JAW}.npy", distance_map)
+        np.save(f"../data/{DIR}/{id}/distance_map_{JAW}.npy", distance_map)