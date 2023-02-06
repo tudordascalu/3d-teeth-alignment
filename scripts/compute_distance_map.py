5488c1645410b6d5fa376728183eaf1a5606b56c
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-06 19:12:28 +0100

    Computes distances

--- scripts/compute_distance_map.py
+++ scripts/compute_distance_map.py
@@ -4,15 +4,16 @@ Computes the distance between each tooth-tooth pair.
 import glob
 
 import numpy as np
+from tqdm import tqdm
 
 from scripts.utils.distance_mapper import DistanceMapper
 
 if __name__ == "__main__":
-    JAW = "lower"
+    JAW = "upper"
     ids = list(map(lambda x: x.split("/")[-1], glob.glob("../data/processed/*")))
     distance_mapper = DistanceMapper()
-    for id in ids:
+    for id in tqdm(ids, total=len(ids)):
         centroids = np.load(f"../data/final/{id}/centroids_{JAW}.npy")
         labels = np.load(f"../data/final/{id}/labels_{JAW}.npy")
         distance_map = distance_mapper(centroids, labels)
-        break
+        np.save(f"../data/final/{id}/distance_map_{JAW}.npy", distance_map)
