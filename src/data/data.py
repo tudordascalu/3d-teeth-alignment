0d75ba18a012077d00855e938ead87fef43949da
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-08 13:40:36 +0100

    Refactors labels

--- src/data/data.py
+++ src/data/data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from torch.nn.functional import one_hot
 
 
 class AlignmentDataset(torch.utils.data.Dataset):
@@ -14,8 +15,8 @@ class AlignmentDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         score_map = torch.from_numpy(np.load(f"{self.data_path}{self.ids[idx]}/score_map_{self.jaw}.npy")).type(
             torch.float32)
-        labels = torch.from_numpy(np.load(f"{self.data_path}{self.ids[idx]}/labels_{self.jaw}.npy")).type(torch.float32)
-
+        labels = torch.from_numpy(np.load(f"{self.data_path}{self.ids[idx]}/labels_{self.jaw}.npy")).type(torch.int64)
+        labels = one_hot(labels, num_classes=17).squeeze(1).type(torch.float32)
         return score_map, labels
 
     def __len__(self):
