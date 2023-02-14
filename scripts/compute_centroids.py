ee63aa51e7ffcfb6ee811435b7e8b584e0e665f8
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-14 12:39:56 +0100

    Defines dummy tooth detector

--- scripts/compute_centroids.py
+++ scripts/compute_centroids.py
@@ -7,12 +7,54 @@ import os
 
 import numpy as np
 import trimesh
-from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
 
 from scripts.utils import arg_parser
 from scripts.utils.centroid_mapper import CentroidMapper
 from scripts.utils.tooth_label_encoder import ToothLabelEncoder
+
+
+class GumRemover:
+    def __init__(self, gum_cls=0):
+        self.gum_cls = gum_cls
+
+    def __call__(self, tooth_labels, instance_labels, vertices):
+        """
+
+        :param tooth_labels: np.array of shape (n_vertices,)
+        :param isntance_labels: np.array of shape (n_vertices,)
+        :param vertices: np.array of shape (n_vertices, 3)
+        :return: tooth_labels_processed, instance_labels_processed, vertices_processed
+        """
+        i_gum_list = tooth_labels == self.gum_cls
+        tooth_labels_processed = np.delete(tooth_labels, i_gum_list)
+        instance_labels_processed = np.delete(instance_labels, i_gum_list)
+        vertices_processed = np.delete(vertices, i_gum_list, axis=0)
+        return tooth_labels_processed, instance_labels_processed, vertices_processed
+
+
+class DummyToothDetector:
+    def __init__(self, dummy_cls=16):
+        self.dummy_cls = dummy_cls
+
+    def __call__(self, tooth_labels, instance_labels):
+        """
+
+        :param tooth_labels: np.array of shape (n_vertices,)
+        :param instance_labels: np.array of shape (n_vertices,)
+        :return: tooth_labels_processed
+        """
+        # Find dummy tooth and assign it label 17
+        tooth_labels_unique = np.unique(tooth_labels)
+        # Iterate over each unique label
+        for tooth_label in tooth_labels_unique:
+            tooth_instance_labels = instance_labels[tooth_labels == tooth_label]
+            tooth_instance_labels_unique = np.unique(tooth_instance_labels)
+            # If there are exactly two unique instance labels, it means there is a dummy tooth
+            if len(tooth_instance_labels_unique) == 2:
+                tooth_labels[instance_labels == tooth_instance_labels_unique[-1]] = self.dummy_cls
+        return tooth_labels
+
 
 if __name__ == "__main__":
     # Parse args
@@ -20,7 +62,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     jaw = "lower"
     n_teeth = args.teeth
-
     encoder = ToothLabelEncoder.encoder(jaw)
     centroid_mapper = CentroidMapper(n_teeth)
     # Compute patient ids
@@ -39,22 +80,13 @@ if __name__ == "__main__":
         instance_labels = np.array(labels["instances"])
         tooth_labels = np.array(labels["labels"])
         # Remove gums
-        i_gum = tooth_labels == 0
-        instance_labels = np.delete(instance_labels, i_gum)
-        tooth_labels = np.delete(tooth_labels, i_gum)
-        vertices = mesh.vertices
-        vertices = np.delete(vertices, i_gum, axis=0)
+        gum_remover = GumRemover(0)
+        tooth_labels, instance_labels, vertices = gum_remover(tooth_labels, instance_labels, mesh.vertices)
+        # Encode labels
         tooth_labels = encoder.transform(tooth_labels)
-        # Find dummy tooth and assign it label 17
-        tooth_labels_unique = np.unique(tooth_labels)
-        # Iterate over each unique label
-        for tooth_label in tooth_labels_unique:
-            tooth_instance_labels = instance_labels[tooth_labels == tooth_label]
-            tooth_instance_labels_unique = np.unique(tooth_instance_labels)
-            # If there are exactly two unique instance labels, it means there is a dummy tooth
-            # Assign the label 16 to the dummy tooth
-            if len(tooth_instance_labels_unique) == 2:
-                tooth_labels[instance_labels == tooth_instance_labels_unique[-1]] = n_teeth - 1
+        # Overwrite dummy tooth
+        dummy_tooth_detector = DummyToothDetector(n_teeth - 1)
+        tooth_labels = dummy_tooth_detector(tooth_labels, instance_labels)
         # Find the tooth centroids of the jaw mesh, given the vertices
         centroids = centroid_mapper(vertices, tooth_labels)
         if not os.path.exists(f"../data/processed/{id}"):
--- tests/scripts/test_compute_centroids.py
+++ tests/scripts/test_compute_centroids.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from scripts.compute_centroids import GumRemover
+from scripts.compute_centroids import GumRemover, DummyToothDetector
 
 
 class TestGumRemover(unittest.TestCase):
@@ -15,6 +15,22 @@ class TestGumRemover(unittest.TestCase):
         np.testing.assert_array_equal(tooth_labels_processed, np.array([1, 2, 1]))
         np.testing.assert_array_equal(instance_labels_processed, np.array([1, 1, 1]))
         np.testing.assert_array_equal(vertices_processed, np.array([[1, 2, 3], [4, 5, 6], [13, 14, 15]]))
+
+
+class TestDummyToothDetector(unittest.TestCase):
+    def test_detect_dummy_tooth(self):
+        # Test case where there is a dummy tooth present
+        dummy_detector = DummyToothDetector(dummy_cls=17)
+        tooth_labels = np.array([1, 2, 2, 2, 2, 4, 5, 6])
+        instance_labels = np.array([1, 2, 2, 3, 3, 4, 5, 6])
+        tooth_labels_processed = dummy_detector(tooth_labels, instance_labels)
+        np.testing.assert_array_equal(tooth_labels_processed, np.array([1, 2, 2, 17, 17, 4, 5, 6]))
+
+        # Test case where there is no dummy tooth present
+        tooth_labels = np.array([11, 12, 13, 14, 15, 16])
+        instance_labels = np.array([1, 2, 3, 4, 5, 6])
+        tooth_labels_processed = dummy_detector(tooth_labels, instance_labels)
+        np.testing.assert_array_equal(tooth_labels_processed, np.array([11, 12, 13, 14, 15, 16]))
 
 
 if __name__ == '__main__':
