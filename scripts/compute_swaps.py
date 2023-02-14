4d58deddde3d88688cd930171ca20d53f063ef30
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-14 10:13:19 +0100

    Uses argument for number of teeth

--- scripts/compute_swaps.py
+++ scripts/compute_swaps.py
@@ -50,13 +50,14 @@ if __name__ == "__main__":
     jaw = args.jaw
     max_swaps = args.swaps
     n = args.n_samples
+    n_teeth = args.teeth
 
     ids = list(map(lambda x: x.split("/")[-1], glob.glob("../data/processed/*")))
     tooth_swapper = ToothSwapper(max_swaps)
     for id in tqdm(ids, total=len(ids)):
         for i in range(n):
             centroids = np.load(f"../data/processed/{id}/centroids_{jaw}.npy")
-            centroids, labels = tooth_swapper(centroids, np.arange(0, 17))
+            centroids, labels = tooth_swapper(centroids, np.arange(0, n_teeth))
             if not os.path.exists(f"../data/final/{id}"):
                 os.mkdir(f"../data/final/{id}")
             np.save(f"../data/final/{id}/centroids_{jaw}_{i}.npy", centroids)
