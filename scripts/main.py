a6b328a79058d91d99a571f6a50f1179722e9787
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-14 10:11:56 +0100

    Adds score map computation in list of scripts to run

--- scripts/main.py
+++ scripts/main.py
@@ -2,12 +2,19 @@ import subprocess
 
 if __name__ == "__main__":
     # The scripts in the order that they should run
-    scripts = ["compute_centroids.py -j lower", "compute_centroids.py -j upper",
+    scripts = ["compute_centroids.py -j lower",
+               "compute_centroids.py -j upper",
                "compute_splits.py",
-               "compute_swaps.py -j lower -s 3", "compute_swaps.py -j upper -s 3",
-               "compute_distance_map.py -d processed -j lower", "compute_distance_map.py -d processed -j upper",
-               "compute_distance_map.py -d final -j lower", "compute_distance_map.py -d final -j upper",
-               "compute_statistics.py -j lower", "compute_statistics.py -j upper"
+               "compute_swaps.py -j lower -s 3",
+               "compute_swaps.py -j upper -s 3",
+               "compute_distance_map.py -d processed -j lower",
+               "compute_distance_map.py -d processed -j upper",
+               "compute_distance_map.py -d final -j lower",
+               "compute_distance_map.py -d final -j upper",
+               "compute_statistics.py -j lower",
+               "compute_statistics.py -j upper",
+               "compute_score_map.py -j lower",
+               "compute_score_map.py -j upper"
                ]
     for script in scripts:
         subprocess.run(["python", script])
