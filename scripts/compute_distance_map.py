f8da76d5d7a64f0ae59e9d19d4c629c96089afa6
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-14 09:49:58 +0100

    Simplifies processed and final dir type handling

--- scripts/compute_distance_map.py
+++ scripts/compute_distance_map.py
@@ -18,13 +18,16 @@ if __name__ == "__main__":
     ids = list(map(lambda x: x.split("/")[-1], glob.glob(f"../data/{dir}/*")))
     distance_mapper = DistanceMapper()
     for id in tqdm(ids, total=len(ids)):
-        for i in range(n):
-            centroids = np.load(f"../data/{dir}/{id}/centroids_{jaw}_{i}.npy")
-            try:
-                labels = np.load(f"../data/{dir}/{id}/labels_{jaw}_{i}.npy")
-            except:
-                if dir == "final":
-                    raise ValueError(f"The labels do not exist for patient {id}.")
-                labels = np.arange(17)
+        if dir == "processed":
+            centroids = np.load(f"../data/{dir}/{id}/centroids_{jaw}.npy")
+            labels = np.arange(17)
             distance_map = distance_mapper(centroids, labels)
-            np.save(f"../data/{dir}/{id}/distance_map_{jaw}_{i}.npy", distance_map)
+            np.save(f"../data/{dir}/{id}/distance_map_{jaw}.npy", distance_map)
+            np.save(f"../data/{dir}/{id}/distance_map_{jaw}.npy", distance_map)
+        elif dir == "final":
+            for i in range(n):
+                centroids = np.load(f"../data/{dir}/{id}/centroids_{jaw}_{i}.npy")
+                labels = np.load(f"../data/{dir}/{id}/labels_{jaw}_{i}.npy")
+                distance_map = distance_mapper(centroids, labels)
+                np.save(f"../data/{dir}/{id}/distance_map_{jaw}_{i}.npy", distance_map)
+                np.save(f"../data/{dir}/{id}/distance_map_{jaw}_{i}.npy", distance_map)
