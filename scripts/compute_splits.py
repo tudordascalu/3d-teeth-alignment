37b1d583bbd1cd4b60f5fc10e562af0969f305fc
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-14 11:18:48 +0100

    Fixes validation set not used

--- scripts/compute_splits.py
+++ scripts/compute_splits.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     ids_train, ids_val = train_test_split(ids_train, train_size=449, random_state=42)
     # Distribute double teeth patients
     ids_train = np.concatenate((ids_train, [ids_double_tooth[0]]))
-    ids_val = np.concatenate((ids_train, [ids_double_tooth[1]]))
+    ids_val = np.concatenate((ids_val, [ids_double_tooth[1]]))
     ids_test = np.concatenate((ids_test, ids_double_tooth[2:]))
     np.save("../data/split/ids_train.npy", ids_train)
     np.save("../data/split/ids_val.npy", ids_val)
