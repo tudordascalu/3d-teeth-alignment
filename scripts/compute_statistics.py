3b09657e83b4a6445193e1fd4df243962b1f7a72
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-14 10:11:08 +0100

    Removes missing teeth from statistics computation

--- scripts/compute_statistics.py
+++ scripts/compute_statistics.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     parser = arg_parser.create_parser()
     args = parser.parse_args()
     jaw = args.jaw
+    n_teeth = args.teeth
     ids = np.load("../data/split/ids_train.npy")
     distance_map_cov_mapper = DistanceMapCov()
     distance_map_acc = []
@@ -36,12 +37,20 @@ if __name__ == "__main__":
         distance_map = np.load(f"../data/processed/{id}/distance_map_{jaw}.npy")
         distance_map_acc.append(distance_map)
     distance_map_acc = np.array(distance_map_acc)
-    distance_map_mean = np.mean(distance_map_acc, axis=0)
-    distance_map_std = np.std(distance_map_acc, axis=0)
+    distance_dict = {}
+    distance_map_mean = np.zeros((n_teeth, n_teeth, 3))
+    distance_map_std = np.zeros((n_teeth, n_teeth, 3))
+    for i in range(17):
+        for j in range(17):
+            distances = distance_map_acc[:, i, j, ...]
+            distances = distances[(distances != np.array([0, 0, 0])).all(axis=1)]
+            if len(distances) > 0:
+                distance_map_mean[i, j] = np.mean(distances, axis=0)
+                distance_map_std[i, j] = np.std(distances, axis=0)
     # Uncomment the following in order to fit a custom distribution (e.g Laplace) to the data
     # distance_map_mean_std_mapper = DistanceMapMeanStdMapper(dist=laplace, n_teeth=n_teeth)
     # distance_map_mean, distance_map_std = distance_map_mean_std_mapper(distance_map_acc)
-    distance_map_cov = distance_map_cov_mapper(distance_map_acc)
+    # distance_map_cov = distance_map_cov_mapper(distance_map_acc)
     np.save(f"../data/statistics/distance_map_mean_{jaw}.npy", distance_map_mean)
     np.save(f"../data/statistics/distance_map_std_{jaw}.npy", distance_map_std)
-    np.save(f"../data/statistics/distance_map_cov_{jaw}.npy", distance_map_cov)
+    # np.save(f"../data/statistics/distance_map_cov_{jaw}.npy", distance_map_cov)
