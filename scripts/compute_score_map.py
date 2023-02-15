bcf1b7ae5f78bb5dc2ba22e7bb137deddff45135
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-15 10:49:32 +0100

    Removes unnecessary data

--- scripts/compute_score_map.py
+++ scripts/compute_score_map.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     # Parse args
     parser = arg_parser.create_parser()
     args = parser.parse_args()
+    print(args)
     jaw = args.jaw
     n_teeth = args.teeth
     mode = args.score_mode
@@ -20,7 +21,6 @@ if __name__ == "__main__":
     # Load statistics
     distance_map_mean = np.load(f"../data/statistics/distance_map_mean_{jaw}.npy")
     distance_map_std = np.load(f"../data/statistics/distance_map_std_{jaw}.npy")
-    distance_map_cov = np.load(f"../data/statistics/distance_map_cov_{jaw}.npy")
     score_mapper = ScoreMapper(n_teeth)
     # Compute ids
     ids = list(map(lambda x: x.split("/")[-1], glob.glob(f"../data/final/*")))
@@ -28,5 +28,5 @@ if __name__ == "__main__":
         for i in range(n):
             centroids = np.load(f"../data/final/{id}/centroids_{jaw}_{i}.npy")
             distance_map = np.load(f"../data/final/{id}/distance_map_{jaw}_{i}.npy")
-            score_map = score_mapper(distance_map, distance_map_mean, distance_map_std, distance_map_cov, centroids)
+            score_map = score_mapper(distance_map, distance_map_mean, distance_map_std, None, centroids)
             np.save(f"../data/final/{id}/score_map_{jaw}_{i}.npy", score_map)
