5e45840626773015fd1a6688bee8cc0967d7237b
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-15 10:36:41 +0100

    fixes tooth swapper

--- scripts/compute_swaps.py
+++ scripts/compute_swaps.py
@@ -30,16 +30,21 @@ class ToothSwapper:
         """
         i_swapped_acc = []
         n_swaps = np.random.randint(0, self.max_swaps + 1)
-        for _ in range(n_swaps):
-            i_list = np.arange(0, self.n_teeth)
-            i_list = np.delete(i_list, i_swapped_acc)
-            i_1 = np.random.choice(i_list, size=1)[0]
-            neighbors = np.arange(max(i_1 - self.neighbor_range, 0), min(i_1 + self.neighbor_range + 1, self.n_teeth))
-            neighbors = np.delete(neighbors,
-                                  np.concatenate((np.where(neighbors == i_1)[0], i_swapped_acc)).astype(np.int32))
-            i_2 = np.random.choice(neighbors, size=1)[0]
-            centroids, labels = self._swap(centroids, labels, i_1, i_2)
-            i_swapped_acc.extend([i_1, i_2])
+        j = 0
+        while j < n_swaps:
+            try:
+                i_list = np.arange(0, self.n_teeth)
+                i_list = np.delete(i_list, i_swapped_acc)
+                i_1 = np.random.choice(i_list, size=1)[0]
+                neighbors = np.arange(max(i_1 - self.neighbor_range, 0),
+                                      min(i_1 + self.neighbor_range + 1, self.n_teeth))
+                neighbors = neighbors[~np.in1d(neighbors, np.concatenate((i_swapped_acc, [i_1])))]
+                i_2 = np.random.choice(neighbors, size=1)[0]
+                centroids, labels = self._swap(centroids, labels, i_1, i_2)
+                i_swapped_acc.extend([i_1, i_2])
+                j += 1
+            except:
+                print(f"One exception! i_1 {i_1}, i_swapped_acc {i_swapped_acc}")
         return centroids, labels
 
     @staticmethod
@@ -57,13 +62,14 @@ class ToothSwapper:
 if __name__ == "__main__":
     parser = arg_parser.create_parser()
     args = parser.parse_args()
+    print(args)
     jaw = args.jaw
     max_swaps = args.swaps
     n = args.n_samples
     n_teeth = args.teeth
 
     ids = list(map(lambda x: x.split("/")[-1], glob.glob("../data/processed/*")))
-    tooth_swapper = ToothSwapper(max_swaps)
+    tooth_swapper = ToothSwapper(n_teeth=n_teeth, neighbor_range=2, max_swaps=max_swaps)
     for id in tqdm(ids, total=len(ids)):
         for i in range(n):
             centroids = np.load(f"../data/processed/{id}/centroids_{jaw}.npy")
