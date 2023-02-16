194e7e9bd7112b2f5088032c7393dea7bff63d12
Tudor Dascalu <tudordascalu1@gmail.com>
2023-02-16 14:20:48 +0100

    Fixes dummy tooth generator

--- scripts/utils/dummy_tooth_generator.py
+++ scripts/utils/dummy_tooth_generator.py
@@ -1,14 +1,24 @@
+from sklearn.decomposition import PCA
+
 from scripts.utils.missing_teeth_detector import MissingTeethDetector
 import numpy as np
 import plotly.express as px
 
 
 class DummyToothGenerator:
-    def __init__(self, n_teeth=17, min_dist=5, max_dist=15):
+    def __init__(self, n_teeth=17, min_dist=5, max_dist=15, ):
+        """
+
+        :param axial_plane: np.array of shape (n_points, n_dim) including points belonging to the plane
+        :param n_teeth:
+        :param min_dist:
+        :param max_dist:
+        """
         self.n_teeth = n_teeth
         self.missing_teeth_detector = MissingTeethDetector()
         self.min_dist = min_dist
         self.max_dist = max_dist
+        self.axial_plane = AxialPlane()
 
     def __call__(self, centroids):
         # Find present teeth
@@ -17,48 +27,94 @@ class DummyToothGenerator:
         tooth_labels = tooth_labels[~np.in1d(tooth_labels, missing_tooth_labels)]
         # Chose one random tooth that has neighbors both to the left and to the right
         i_tooth = np.random.choice(np.arange(1, len(tooth_labels) - 1), size=1)[0]
-        i_tooth = 2
-        tooth = tooth_labels[i_tooth]
-        neighbor_l = tooth_labels[i_tooth - 1]
-        neighbor_r = tooth_labels[i_tooth + 1]
-        print(f"tooth {tooth}, neighbor_l {neighbor_l}, neighbor_r {neighbor_r}")
-        # Find the coordinates of the vectors corresponding to the centroids of the tooth, neighbor_l and neighbor_r
-        p = centroids[tooth]
-        a = centroids[neighbor_l]
-        b = centroids[neighbor_r]
+        # Find tooth and neighbor coordinates
+        p = centroids[tooth_labels[i_tooth]]
+        a = centroids[tooth_labels[i_tooth - 1]]
+        b = centroids[tooth_labels[i_tooth + 1]]
+        # Find axial plane
+        self.axial_plane.fit(centroids[tooth_labels])
+        # Project tooth and neighbor centroids on axial plane
+        p = self.axial_plane.project(p)
+        a = self.axial_plane.project(a)
+        b = self.axial_plane.project(b)
+        # Find the vector pointing in the direction perpendicular to the line formed by the neighbors
         ab = b - a
         ap = p - a
         proj_ap = np.dot(ap, ab) / np.dot(ab, ab) * ab
         intersection = a + proj_ap
         intp = (p - intersection) / np.linalg.norm(p - intersection)
-        dummy_tooth_centroid = p + 5 * intp
-
-        # # Find the vector unit vector pointing along the line defined by neighbor_r_centroid-neighbor_l_centroid
-        # u = (neighbor_r_centroid - neighbor_l_centroid) / np.linalg.norm((neighbor_r_centroid - neighbor_l_centroid))
-        # # Find the vector resulting from projecting the tooth_centroid vector onto the unit vector u
-        # v = np.dot(u, tooth_centroid) * u
-        # # Find the unit vector that is perpendicular to v passing through the tooth_centroid
-        # w = (tooth_centroid - v) / np.linalg.norm(tooth_centroid - v)
-        # # Generate dummy_tooth_centroid by moving the tooth_centroid by alpha mm in the direction of w
-        # # alpha = np.random.choice(np.arange(self.min_dist, self.max_dist, step=.1), size=1)[0]
-        # alpha = 5
-        # print(f"alpha  {alpha}")
-        # dummy_tooth_centroid = tooth_centroid + alpha * w
-        centroids[-1] = dummy_tooth_centroid
+        # Randomly chose how many mm away to place the tooth
+        alpha = np.random.choice(np.arange(5, 10, step=.1), size=1)[0]
+        # Check if tooth sits outside the line formed by the neighbors compared to the centroid
+        if self.is_outer_tooth(p, self.axial_plane.plane[-1], a, b):
+            dummy_tooth_centroid = p + alpha * intp
+        else:
+            dummy_tooth_centroid = p - alpha * intp
+        # Randomly offset dummy_tooth_centroids by maximum of 1mm in the X, Y, Z axes
+        offset = np.random.choice(np.arange(-2, 2.1, step=.1), size=3)
+        # Set the centroid for the dummy tooth placeholder
+        centroids[-1] = dummy_tooth_centroid + offset
         return centroids
+
+    def is_outer_tooth(self, p, q, a, b):
+        """
+        This method assumes that the points p, q, a, b are coplanar.
+
+        :param p: coordinates of tooth
+        :param q: coordinates of centroid
+        :param a: coordinates of neighbour 1
+        :param b: coordinates of neigbhour 2
+        :return: true if the tooth sits opposite from the centroid w.r.t the line formed by the neighbors, false otherwise
+        """
+        # Compute the direction of the line
+        line_dir = b - a
+        # Compute the cross product of the line direction and the vector from a to p (or q)
+        p_cross = np.cross(line_dir, p - a)
+        q_cross = np.cross(line_dir, q - a)
+        # Check if the dot products of the cross products are negative
+        if np.dot(p_cross, q_cross) < 0:
+            return True
+        else:
+            return False
+
+
+class AxialPlane:
+    def __init__(self):
+        self.plane = None
+
+    def fit(self, centroids):
+        centroids_mean = np.mean(centroids, axis=0)
+        pca = PCA()
+        pca.fit(centroids)
+        pcs, variance = pca.components_, pca.explained_variance_
+        pcs += centroids_mean
+        self.plane = [pcs[0], pcs[1], centroids_mean]
+
+    def project(self, p):
+        a, b, c = self.plane
+        # Calculate the normal vector of the plane
+        n = np.cross(b - a, c - a)
+        # Calculate the vector from point A to point P
+        v = p - a
+        # Calculate the projection of vector v onto the normal vector of the plane
+        proj = v - np.dot(v, n) / np.dot(n, n) * n
+        # Calculate the projected point Q by adding the projection of vector v to point A
+        q = a + proj
+        return q
 
 
 if __name__ == "__main__":
-    centroids = np.load("../../data/processed/0EAKT1CU/centroids_lower.npy")
+    centroids = np.load("../../data/processed/0KPHM46Q/centroids_lower.npy")
     dummy_tooth_generator = DummyToothGenerator(n_teeth=17)
     centroids = dummy_tooth_generator(centroids)
     # Visualization
     centroids = centroids[np.where((centroids != np.array([0, 0, 0])).all(axis=1))[0]]
+    a, b, c = dummy_tooth_generator.axial_plane.plane
     fig = px.scatter_3d(x=centroids[:, 0], y=centroids[:, 1], z=centroids[:, 2])
-    # fig.add_trace(px.line_3d(x=[tooth_centroid[0], centroids[-1][0]],
-    #                          y=[tooth_centroid[1], centroids[-1][1]],
-    #                          z=[tooth_centroid[2], centroids[-1][2]]).data[0])
-    # fig.add_trace(px.line_3d(x=[neighbor_l_centroid[0], neighbor_r_centroid[0]],
-    #                          y=[neighbor_l_centroid[1], neighbor_r_centroid[1]],
-    #                          z=[neighbor_l_centroid[2], neighbor_r_centroid[2]]).data[0])
+    fig.add_trace(px.line_3d(x=[c[0], a[0]],
+                             y=[c[1], a[1]],
+                             z=[c[2], a[2]]).data[0])
+    fig.add_trace(px.line_3d(x=[c[0], b[0]],
+                             y=[c[1], b[1]],
+                             z=[c[2], b[2]]).data[0])
     fig.show()
