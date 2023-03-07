710f93f784f84a7f004cfa85aff3f49e5df9f0c9
Tudor Dascalu <tudordascalu1@gmail.com>
2023-03-07 14:52:59 +0100

    Fixes prediction loop

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,9 @@
+import os
+
 import numpy as np
 import yaml
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from src.data.data import AlignmentDataset
 from src.model.model import AlignmentNet
@@ -12,41 +15,30 @@ if __name__ == "__main__":
     # Data
     ids = np.load("data/split/ids_test.npy")
     # Define dataset
-    dataset_args = dict(data_path=config["data_path"], jaw="lower")
+    dataset_args = dict(data_path=config["data_path"], jaw=config["jaw"], n_samples=config["n_samples"])
     dataset = AlignmentDataset(ids=ids, **dataset_args)
     loader_args = dict(batch_size=config["batch_size"], num_workers=0, pin_memory=True)
     loader_test = DataLoader(dataset, **loader_args)
-    model = AlignmentNet.load_from_checkpoint("checkpoints/version_147/checkpoints/epoch=28-step=28.ckpt")
+    model = AlignmentNet.load_from_checkpoint("checkpoints/version_148/checkpoints/epoch=39-step=39.ckpt")
     assignment_solver = AssignmentSolver()
 
     y_acc = []
     y_pred_acc = []
     y_pred_default_acc = []
-    for i, id in enumerate(ids):
-        x, y = dataset[i]
-        centroids = np.load(f"data/final/{id}/centroids_lower_0.npy")
-        present_teeth = np.where((centroids != np.array([0, 0, 0])).all(axis=1))[0]
-        y_pred = model(x.unsqueeze(0)).clone().detach().numpy()
-        y_pred, _ = assignment_solver(y_pred)
-        y_pred = y_pred[0]
-        y = y.argmax(0).numpy()
-        y_pred_default = np.arange(17)
-        i_delete_acc = []
-        # for ii, tooth in enumerate(y):
-        #     if tooth not in present_teeth:
-        #         i_delete_acc.append(ii)
-        # y = np.delete(y, i_delete_acc)
-        # y_pred = np.delete(y_pred, i_delete_acc)
-        y_pred_default = np.delete(y_pred_default, i_delete_acc)
-        y_acc.extend(y)
-        y_pred_acc.extend(y_pred)
-        y_pred_default_acc.extend(y_pred_default)
-        print(f"-- {id}")
-        print(f"y {y}")
-        print(f"p {y_pred}")
-
-    y = np.array(y_acc)
-    y_pred_acc = np.array(y_pred_acc)
-    y_pred_default_acc = np.array(y_pred_default_acc)
-    print(f"AI: {sum(y == y_pred_acc) / len(y_pred_acc)}")
-    print(f"Default: {sum(y == y_pred_default_acc) / len(y_pred_default_acc)}")
+    for i, id in tqdm(enumerate(ids), total=len(ids)):
+        # Create directory for patient
+        if not os.path.exists(f"output/aligner/{id}"):
+            os.mkdir(f"output/aligner/{id}")
+        # Loop through all samples
+        for j in range(config["n_samples"]):
+            x, y = dataset[i + j]
+            y = y.argmax(0).numpy()
+            centroids = np.load(f"data/final/{id}/centroids_{config['jaw']}_{j}.npy")
+            y_pred = model(x.unsqueeze(0)).clone().detach().numpy()
+            y_pred, _ = assignment_solver(y_pred)
+            y_pred = y_pred[0]
+            print(f"y: {y}")
+            print(f"y_pred: {y_pred}")
+            np.save(f"output/aligner/{id}/aligned_labels_pred_{config['jaw']}_{j}.npy", y_pred)
+            np.save(f"output/aligner/{id}/aligned_labels_{config['jaw']}_{j}.npy", y)
+            np.save(f"output/aligner/{id}/centroids_{config['jaw']}_{j}.npy", centroids)
