a94e72b7141eea5828cd7f5f41be364c9b759188
Tudor Dascalu <tudordascalu1@gmail.com>
2023-03-07 18:16:57 +0100

    Refactors argparser

--- scripts/utils/arg_parser.py
+++ scripts/utils/arg_parser.py
@@ -8,12 +8,10 @@ def create_parser():
     parser = argparse.ArgumentParser(description="The script handles pre-processing of the data.")
     parser.add_argument("-j", "--jaw", type=str, help="The jaw to focus on.", default="upper",
                         choices=["lower", "upper"])
-    parser.add_argument("-d", "--dir", type=str, help="The type of data to act on.", default="processed",
+    parser.add_argument("-d", "--dir", type=str, help="The type of data to act on.", default="final",
                         choices=["processed", "final"])
     parser.add_argument("-s", "--max_swaps", type=int, help="Maximum number of swaps.", default=2)
     parser.add_argument("-t", "--teeth", type=int, help="Number of teeth.", default=17)
-    parser.add_argument("-sm", "--score_mode", type=str, help="The score mode", default="univariate",
-                        choices=["univariate", "multivariate"])
     parser.add_argument("-n", "--n_samples", type=int, help="The number of samples created for each patient.",
                         default=10)
     parser.add_argument("-pd", "--p_dummy", type=float, help="The probability of injecting a dummy tooth.",
@@ -24,4 +22,7 @@ def create_parser():
                         default=.5)
     parser.add_argument("-mm", "--max_missing", type=int, help="The probability of removing a tooth.",
                         default=4)
+    parser.add_argument("-md", "--max_distance", type=int,
+                        help="The maximum distance between predicted and ground truth centroids.",
+                        default=3)
     return parser
